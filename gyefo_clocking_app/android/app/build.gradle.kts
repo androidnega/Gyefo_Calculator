06661d767f90d05133af1bb8e9b00dc5c5d124ba
androidnega <kwofiee3@gmail.com>
2025-06-16 19:57:52 +0000

    built apk successful

--- gyefo_clocking_app/android/app/build.gradle.kts
+++ gyefo_clocking_app/android/app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 android {
     namespace = "com.example.gyefo_clocking_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
+        isCoreLibraryDesugaringEnabled = true    }
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.gyefo_clocking_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -38,8 +38,11 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
-        }
-    }
+        }    }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.2.2")
 }
 
 flutter {
